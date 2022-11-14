97af99931c6f4ab8a89fbe482caa4d8fa51c2e1a
Drew Malone <drew.malone@gmail.com>
2022-11-14 09:52:37 -0500

    add www to siteurl to troubleshoot DNS

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Drew Malone'
 SITENAME = 'Tactical Programming'
-SITEURL = 'https://tacticalprogramming.com'
+SITEURL = 'https://www.tacticalprogramming.com'
 
 PATH = 'content'
 
